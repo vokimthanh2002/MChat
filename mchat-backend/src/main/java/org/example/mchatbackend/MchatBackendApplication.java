2c9af594ea0e6ae57ec8c023d42d0cee418915c1
DacHaiPham <hai.for.work4320@gmail.com>
2024-07-31 16:02:23 +0700

    DacHai update

--- mchat-backend/src/main/java/org/example/mchatbackend/MchatBackendApplication.java
+++ mchat-backend/src/main/java/org/example/mchatbackend/MchatBackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MchatBackendApplication {
-
+//hello
     public static void main(String[] args) {
         SpringApplication.run(MchatBackendApplication.class, args);
     }
